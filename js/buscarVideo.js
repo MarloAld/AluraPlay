71b8cc2588e4ff964bb84c20f0c9409aead113db
Marlinton Aldrighi <marlinton13@gmail.com>
2024-05-23 11:20:04 -0300

    Final version

---
+++ js/buscarVideo.js
@@ -0,0 +1,21 @@
+import { conectaApi } from "./conectaAPI.js";
+import constroiCard from "./mostrarVideos.js";
+
+async function buscarVideo(evento){
+    evento.preventDefault()
+    
+    const dadosDePesquisa = document.querySelector("[data-pesquisa]").value
+    const busca = await conectaApi.buscaVideo(dadosDePesquisa)
+    const lista = document.querySelector("[data-lista]")
+
+    lista.innerHTML = ''
+    busca.forEach(elemento => lista.appendChild(constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
+
+    if(busca.length == 0){
+        lista.innerHTML = `<h2 class="mensagem__titulo">Não foram encontrados vídeos</h2>`
+    }
+}
+
+const botaoDePesquisa = document.querySelector("[data-botao-pesquisa]")
+
+botaoDePesquisa.addEventListener('click', evento => buscarVideo(evento))
